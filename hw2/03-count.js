e691f3cdcea6e07fa2b7072c5cfe4501bd08347a
ellie <elaheh@pdx.edu>
2023-10-14 17:42:01 -0700

    added initial html and js code for count, it searches the input and highlights it

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,18 @@
-// Add your code here
+input = document.getElementById("user-input");
+input.addEventListener("keydown", handleKeyDown);
 
-input.addEventListener('keydown', handleKeyDown);
+function handleKeyDown() {
+  let userInput = document.getElementById("user-input").value;
+  const paragraph = document.getElementById("text").textContent;
+  let text = document.getElementById("text");
+
+  if (paragraph.includes(userInput)) {
+    //creating an regular expression for a global and case-insensitive matching
+    let allWords = new RegExp(`${userInput}`, "gi");
+    //using mark to highlight the searched word.
+    text.innerHTML = text.textContent.replace(
+      allWords,
+      (match) => `<mark>${match}</mark>`
+    );
+  }
+}
