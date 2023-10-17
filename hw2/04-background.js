42f3211436a59b8b25730146d11ca420718f6e22
ellie <elaheh@pdx.edu>
2023-10-17 13:15:02 -0700

    added color chang on load, used rgba for colors, fixed minor issues

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,33 +1,41 @@
 // Add your code here
-
+let input = document.getElementById("userinput");
+const background = document.querySelector("body");
 let button = document.getElementById("button");
 button.addEventListener("click", changColor);
-let input = document.getElementById("input");
 
 let interval;
+window.addEventListener("load", () => {
+  interval = setInterval(function () {
+    background.style.backgroundColor = color();
+  }, 3000);
+  button.value = "Stop";
+  button.style.background = "red";
+});
 
-
-function changColor(){
-    const background = document.querySelector("body");
-    if(button.value === "Start") {
-        interval = setInterval(function() {
-            background.style.backgroundColor = color();
-          }, 1000);
-        button.value = "Stop";
-
+function changColor() {
+  if (button.value === "Start") {
+    if (input) {
+      interval = setInterval(function () {
+        background.style.backgroundColor = color();
+      }, input.value * 1000);
     }
-    else if(button.value === "Stop") {
-        clearInterval(interval);
-        button.value = "Start";
-        background.style.backgroundColor = "blue";
-    }
+
+    button.value = "Stop";
+    button.style.background = "red";
+  } else if (button.value === "Stop") {
+    clearInterval(interval);
+    button.value = "Start";
+    button.style.background = "blue";
+  }
 }
-    function color() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-          color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-      }
-  
+
+function color() {
+  let color = "";
+  for (let i = 0; i < 3; i++) {
+    color += Math.floor(Math.random() * 256);
+    color += ",";
+  }
+  rgbacolor = "rgba(" + color + "0.5)";
+  return rgbacolor;
+}
